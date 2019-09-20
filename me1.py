483828353ecef41321af78c53bb23b490ea55f26
Julian Pistorius <julian@julianpistorius.com>
2019-09-20 09:56:14 -0700

    Made mock extractor 1 input optional for now.

--- me1.py
+++ me1.py
@@ -1,7 +1,7 @@
 """
 Mock Extractor 1
 
-Input: A string indicating the unique name of the workflow run
+Input: A string indicating the unique name of the workflow run (optional)
 Output: Creates an empty numbered file (timestamp based) incorporating the
 input string
 """
